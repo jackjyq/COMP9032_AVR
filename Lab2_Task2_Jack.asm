33d007ce030d002d5bcd1716fabe22ef3cb24c90
Jack Jiang <jackjyq@outlook.com>
2017-08-28 22:36:47 +1000

    lab2_task 2 withou debug

--- Lab2_Task2_Jack.asm
+++ Lab2_Task2_Jack.asm
@@ -2,39 +2,74 @@
 ;Date: 27/08/2017
 ;Version: 1
 ;Description: String to integer convertor
-;
-;
-;Test Sample:
-
-
 .include "m2560def.inc"
 
-.def integerMSB = R16
-.def integerLSB = R17
-.def char0 = R18
-.def char1 = R19
-.def char2 = R20
-.def char3 = R21
-.def char4 = R22
-.equ asciiZero = 0x30
+.def n_MSB = R16
+.def n_LSB = R17
+.def i = R18
+.def c = R19
+.def zero = R20
+.def ten = R21
+
 
 ; code/program memory, constants, starts from 0x0000
                .cseg                       
-string:        .dw 0x3132333435 ; '0' is 0x30, this string is '12345' 
+string:        .dw "12345"
 
-; main
-ldi ZH, high(string << 1)
-ldi ZL, low(string <<1)
 
-lpm char0, Z+
-lpm char1, Z+
-lpm char2, Z+
-lpm char3, Z+
-lpm char4, Z
-rcall atoi
+main:
+    ldi ZH, high(string << 1)   ; initialize Z point to string's address
+    ldi ZL, low(string << 1)
+    clr R16                     ; initialize registers for getting result
+    clr R17
+    rcall atoi
+end:
+    rjmp end
 
-end: rjmp end
 
-; atoi
-atoi:   ret
+atoi:
+    ; Prologue
+    push YL
+    push YH
+    push i
+    push c
+    push zero
+    push ten
+    in YL, SPL
+    in YH, SPH
+    sbiw Y, 0
+    out SPH, YH
+    out SPL, YL
+    ; Function body
+loop:
+    ldi zero, 0     ; initialize some constant value
+    ldi ten, 10
+    lpm c, Z+       ; retrive char in string to c
+    cpi c, '0'
+    brlo return     ; if char is less than '0'
+    cpi c, '9'+1          
+    brsh return     ; if char is greater than '9'
+    cpi n, 65536
+    brsh return     ; if n >= 65536
+    subi c, '0'     ; c = c - '0'
+    mul n_MSB, ten  ; n = n * 10
+    movw n_MSB, R0
+    mul n_LSB, ten
+    movw n_LSB, R0
+    add n_MSB, R1
+    add n_LSB, char ; n = n + c
+    adc n_MSB, zero
+    rjmp loop
+return:
+    ; Epilogue
+    adiw Y, 0
+    out SPH, YH
+    out SPL, YL
+    pop ten
+    pop zero
+    pop c
+    pop i
+    pop YH
+    pop YL
+    ret
 
